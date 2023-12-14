4e072b316aa77053a949b9d8d36f09952f07d55d
Maurílio H Rocha <58280099+MaurilioHRocha@users.noreply.github.com>
2023-12-14 15:05:06 -0300

    Add files via upload

--- SecurityWebCheck.sh
+++ SecurityWebCheck.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
-# Script de Verificação de Configuração de Segurança do Servidor Web
-
-echo "######## Verificação de Segurança do Servidor Web ########"
+ 
+# Script de Verifica    o de Configura    o de Seguran  a do Servidor Web
+ 
+echo "######## Verifica    o de Seguran  a do Servidor Web ########"
 echo
-
-read -p "Digite o endereço IP do servidor web: " ip
-
-# Verifica a versão do servidor web
-echo "[+] Verificando a versão do servidor web..."
+ 
+read -p "Digite o endere  o IP do servidor web: " ip
+ 
+# Verifica a vers  o do servidor web
+echo "[+] Verificando a vers  o do servidor web..."
 curl -I $ip | grep "Server"
-
-# Verifica se o servidor web permite listagem de diretórios
-echo "[+] Verificando se o servidor permite listagem de diretórios..."
+ 
+# Verifica se o servidor web permite listagem de diret  rios
+echo "[+] Verificando se o servidor permite listagem de diret  rios..."
 curl -s $ip | grep "<title>Index of"
-
+ 
 # Verifica se o servidor web possui vulnerabilidades conhecidas
 echo "[+] Verificando vulnerabilidades conhecidas..."
 nmap -p 80 --script http-vuln* $ip
-
-echo "######## FIM *******"
-exit 0
+ 
+echo "## Fim da Verificao ##"
